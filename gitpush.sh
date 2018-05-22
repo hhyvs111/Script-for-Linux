c88d813ae64adc4844e2198b35cdbfbcbf3e138b
tanwenbo <445622919@qq.com>
2018-05-22 09:16:57 +0800

    upload

---
+++ gitpush.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#自己写一个推送代码的脚本，利用刚学的知识
+echo "push start!"
+#推送工作流 
+#一共由三个阶段：工作目录——暂存区（Index，保存临时改动）——HEAD（最后一次提交的结果） 
+#->git add <filename>或者git add *自动判断添加哪些文件，添加文件至暂存区。可以多次反复使用，添加多个文件。 
+#->git commit -m "代码提交信息（更新日志等）"提交文件至HEAD。 
+#->git push origin master提交文件至远端仓库master分支（可改）。
+
+#那么首先运行脚本的情况是在仓库目录，那么就不用判断是否有目录了，但是要判断下当前目录是否创建了仓库
+
+isRepo=$(ls -a | grep .git)
+git=".git"
+
+#if[ $isRepo != $git ]
+if test $isRepo != ".git";
+then
+	echo "This dir is not git repositories!"
+ 	exit 1
+fi
+
+echo "begin add file to cache!"
+git add *
+echo "Enter your message for this commit:"
+read message_commit
+git commit -m $message_commit
+#提交到主分支
+git push origin master
+exit 0
